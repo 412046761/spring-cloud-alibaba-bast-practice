1ea11f1ed09b4277c8e8694ad0d7e9a8404e9306
t-yue.li1 <t-yue.li1@pcitc.com>
2026-03-30 13:42:50 +0800

    init and nacos

--- spring-cloud-alibaba-nacos/src/main/java/com/qf/springcloudalibabanacos/Application.java
+++ spring-cloud-alibaba-nacos/src/main/java/com/qf/springcloudalibabanacos/Application.java
@@ -3,6 +3,12 @@ package com.qf.springcloudalibabanacos;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 端口nacos应用入口
+ *
+ * @author ly
+ * @since 2026-03-05 15:32
+ */
 @SpringBootApplication
 public class Application {
 
